2a470e88643bf1ee1203284a32e5c49568f0c12e
suyanoyata <teikukuheia@gmail.com>
2025-01-24 16:55:34 +0200

    feat: add FLEXTool for iOS debugging

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
 import { useProperties } from "@/store/use-properties";
-import { SafeAreaView, Switch, View } from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
+import { SafeAreaView, Switch, View, NativeModules } from "react-native";
 
 const SwitchMenuOption = ({
   children: text,
@@ -30,6 +32,35 @@ const SwitchMenuOption = ({
   );
 };
 
+const ActionMenuOption = ({
+  children: text,
+  note,
+  actionText,
+  action,
+  buttonVariant,
+  debugOnly = false,
+}: {
+  children: string;
+  note?: string;
+  actionText: string;
+  action: () => void;
+  buttonVariant?: "destructive";
+  debugOnly?: boolean;
+}) => {
+  if (!__DEV__ && debugOnly) return null;
+  return (
+    <View>
+      <View className="flex-row items-center mt-2">
+        <Text className="text-zinc-200 font-medium flex-1">{text}</Text>
+        <Button variant={buttonVariant} size="sm" onPress={action}>
+          {actionText}
+        </Button>
+      </View>
+      {note && <Text className="text-zinc-600 text-sm font-medium mt-1">{note}</Text>}
+    </View>
+  );
+};
+
 export default function Menu() {
   const {
     readerDisplayCurrentPage,
@@ -37,6 +68,10 @@ export default function Menu() {
     showQueryDevTools,
     setShowQueryDevTools,
   } = useProperties();
+
+  const queryClient = useQueryClient();
+
+  const { RNFlipboardFlex } = NativeModules;
 
   return (
     <SafeAreaView className="mx-4 gap-2">
@@ -53,6 +88,21 @@ export default function Menu() {
       >
         Show query dev-tools bubble
       </SwitchMenuOption>
+      <ActionMenuOption
+        buttonVariant="destructive"
+        note="This will clear all previous network requests, such as catalog, search results data, image servers, etc... Your search and reading history will remain."
+        actionText="Clear"
+        action={() => queryClient.clear()}
+      >
+        Clear network caches
+      </ActionMenuOption>
+      <ActionMenuOption
+        actionText="Open"
+        debugOnly
+        action={() => RNFlipboardFlex.showExplorer()}
+      >
+        Open FLEXTool
+      </ActionMenuOption>
       <Text className="text-zinc-700 text-center">
         client version: {process.env.EXPO_PUBLIC_VERSION}
       </Text>
