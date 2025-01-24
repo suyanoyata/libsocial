7a232b10eb625b153b07bb2ae04d9022cb9b3b63
suyanoyata <teikukuheia@gmail.com>
2025-01-24 18:58:51 +0200

    misc: remove bash script that is no longer used

--- scripts/get-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version=$(git rev-parse --short HEAD)
-
-sed -i '' '/^EXPO_PUBLIC_VERSION/d' .env
-echo "EXPO_PUBLIC_VERSION=$version" >> .env
-
-echo "replaced with $version version"
