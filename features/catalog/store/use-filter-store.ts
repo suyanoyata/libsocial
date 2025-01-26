b6c7d2fc95e63be1bea2f14dceb91fdd63bcbbff
suyanoyata <teikukuheia@gmail.com>
2025-01-26 22:54:37 +0200

    feat(genres-filter): set genre filter from other title genres

--- features/catalog/store/use-filter-store.ts
+++ features/catalog/store/use-filter-store.ts
@@ -3,15 +3,23 @@ import { create } from "zustand";
 interface FilterStoreProperties {
   genres: number[];
   addGenre: (genreId: number) => void;
+  resetGenresWithId: (genreId: number) => void;
   removeGenre: (genreId: number) => void;
 }
 
 export const useFilterStore = create<FilterStoreProperties>((set, get) => ({
   genres: [],
   addGenre: (genreId) => {
-    set((state) => ({
-      genres: [...state.genres, genreId],
-    }));
+    set((state) => {
+      if (state.genres.includes(genreId)) return state;
+
+      return {
+        genres: [...state.genres, genreId],
+      };
+    });
+  },
+  resetGenresWithId: (genreId) => {
+    set(() => ({ genres: [genreId] }));
   },
   removeGenre: (genreId) => {
     const filteredGenres = get().genres.filter((id) => id !== genreId);
--- features/title/components/title-genre.tsx
+++ features/title/components/title-genre.tsx
@@ -1,13 +1,24 @@
 import { Pressable } from "react-native";
+import { router } from "expo-router";
 
 import { Text } from "@/components/ui/text";
 
 import { Genre } from "@/features/shared/types/title";
 import { cn } from "@/lib/utils";
 
+import { useFilterStore } from "@/features/catalog/store/use-filter-store";
+
 export const TitleGenre = ({ genre }: { genre: Genre }) => {
+  const { resetGenresWithId } = useFilterStore();
+
+  const handleGenrePress = () => {
+    resetGenresWithId(genre.id);
+    router.dismissTo("/catalog");
+  };
+
   return (
     <Pressable
+      onPress={handleGenrePress}
       className={cn(
         "bg-zinc-800/60 border border-zinc-800 rounded-lg p-1 px-2",
         genre.adult && "bg-red-200/10"
