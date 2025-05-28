e1dbbcd2fb024c0d782f6d1e341a8e1465dc71d5
suyanoyata <teikukuheia@gmail.com>
2025-05-28 12:57:49 +0300

    style: add prettier and eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "@feature-sliced",
+    "@feature-sliced/eslint-config/rules/import-order",
+    "@feature-sliced/eslint-config/rules/layers-slices",
+  ],
+  plugins: ["unused-imports"],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    "import/no-internal-modules": "off",
+    "unused-imports/no-unused-imports": "warn",
+  },
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
   tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  jsxSingleQuote: true,
+  semi: false,
+  // singleQuote: true,
+  // jsxSingleQuote: true,
   printWidth: 90,
+  trailingComma: 'none'
 }
 
 module.exports = config
